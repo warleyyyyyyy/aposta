aacf898efc0fc0efc23ab3edac6f38b39ce7dc36
warleyyyyyyy <warleyhshsjff@gmail.com>
2022-10-25 20:30:39 -0300

    tudo llkklklk

---
+++ vamo.py
@@ -0,0 +1,14 @@
+def menu():
+    
+    while True:
+
+        print("1 cadastro de aulo")
+        print("2 listar todos os alunos")
+        print("3 listar aluno em especifico")
+        print("4 atualizar aluno")
+        print("5 excluir aluno")
+        print("6 media")
+        option =  int (input("digite a opcao escolhida"))
+        if option == 1 :
+            print("igual a 1")
+menu()
